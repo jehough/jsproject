355197ce22c5cc7f5453b2d6ca9572626c207a1b
jehough <teachlife@outlook.com>
2019-10-23 21:03:56 -0400

    adds js files for classes

--- jsproject-frontend/src/index.js
+++ jsproject-frontend/src/index.js
@@ -1,8 +1,19 @@
 const login = document.getElementById('login')
 const new_user = document.getElementById('new_user')
 const button_container = document.getElementById('button_container')
+const new_user_button = document.getElementById('new_user_button')
+const login_button = document.getElementById('login_button')
 
 login.addEventListener('click', function(e){
   button_container.style.visibility = "hidden"
   document.getElementById('login_form').style.visibility = "visible"
+})
+
+new_user.addEventListener('click', function(e){
+  button_container.style.visibility = "hidden"
+  document.getElementById('new_user_form').style.visibility = "visible"
+})
+
+new_user_button.addEventListener('click',function(e){
+  getNewBudget()
 })
