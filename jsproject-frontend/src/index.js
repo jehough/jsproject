79629fdd3d2c2670f66e9cd3bb8a00969279cd01
jehough <teachlife@outlook.com>
2019-10-23 21:16:00 -0400

    adds function to new user button

--- jsproject-frontend/src/index.js
+++ jsproject-frontend/src/index.js
@@ -3,6 +3,8 @@ const new_user = document.getElementById('new_user')
 const button_container = document.getElementById('button_container')
 const new_user_button = document.getElementById('new_user_button')
 const login_button = document.getElementById('login_button')
+const newName = document.getElementById('newName').value
+const userName = document.getElementById('userName').value
 const configObj = {
   method: "POST",
   headers: {
@@ -11,6 +13,10 @@ const configObj = {
   },
   body: JSON.stringify(formData)
 };
+
+function displayBudget(json){
+  console.log(json)
+}
 
 login.addEventListener('click', function(e){
   button_container.style.visibility = "hidden"
@@ -22,6 +28,11 @@ new_user.addEventListener('click', function(e){
   document.getElementById('new_user_form').style.visibility = "visible"
 })
 
-new_user_button.addEventListener('click',function(e){
-  getNewBudget()
-})
+new_user_button.addEventListener('click',()=>{
+  let formData = {
+    name: newName
+  }
+  fetch("http://localhost:3000/users", configObj)
+  .then(resp => resp.json())
+  .then(json => displayBudget(json))
+}}
