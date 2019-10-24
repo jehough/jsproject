ecf70b69cef1c585fa8aeb33e52715f91260942c
jehough <teachlife@outlook.com>
2019-10-23 21:18:52 -0400

    adds functionality to login button

--- jsproject-frontend/src/index.js
+++ jsproject-frontend/src/index.js
@@ -3,8 +3,6 @@ const new_user = document.getElementById('new_user')
 const button_container = document.getElementById('button_container')
 const new_user_button = document.getElementById('new_user_button')
 const login_button = document.getElementById('login_button')
-const newName = document.getElementById('newName').value
-const userName = document.getElementById('userName').value
 const configObj = {
   method: "POST",
   headers: {
@@ -30,9 +28,18 @@ new_user.addEventListener('click', function(e){
 
 new_user_button.addEventListener('click',()=>{
   let formData = {
-    name: newName
+    name: document.getElementById('userName').value
   }
-  fetch("http://localhost:3000/users", configObj)
+  fetch("http://localhost:3000/users/new", configObj)
   .then(resp => resp.json())
   .then(json => displayBudget(json))
 }}
+
+login_button.addEventListener('click',()=>{
+  let formData = {
+    name: document.getElementById('newName').value
+  }
+  fetch("http://localhost:3000/users",configObj)
+  .then(resp => resp.json())
+  .then(json => displayBudget(json))
+})
