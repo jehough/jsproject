a41478218f06e01f29a63ba8a4d96aa1a4684cff
jehough <teachlife@outlook.com>
2019-10-23 21:05:29 -0400

    adds json object to submit form data

--- jsproject-frontend/src/index.js
+++ jsproject-frontend/src/index.js
@@ -3,6 +3,14 @@ const new_user = document.getElementById('new_user')
 const button_container = document.getElementById('button_container')
 const new_user_button = document.getElementById('new_user_button')
 const login_button = document.getElementById('login_button')
+const configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
 
 login.addEventListener('click', function(e){
   button_container.style.visibility = "hidden"
