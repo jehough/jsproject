769947b9b85b8ee92308eaafa26ee0519142ce18
jehough <teachlife@outlook.com>
2019-10-25 10:40:29 -0400

    adds return for transactions

---
+++ jsproject-backend/app/controllers/category_controller.rb
@@ -0,0 +1,7 @@
+class CategoryController < ApplicationController
+
+  def show
+    category = Category.find(params[:id])
+    render json: TransactionSerializer.new(category.transactions)
+  end
+end
--- jsproject-backend/config/routes.rb
+++ jsproject-backend/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/users' => 'users#login'
   post '/users/new' => 'users#new'
+  resources :category, only: [:show]
 end
