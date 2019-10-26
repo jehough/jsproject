11ebe15e7c94a858e5fb7438106f0eb15dab9b9b
jehough <teachlife@outlook.com>
2019-10-26 00:10:32 -0400

    finishes create route

--- jsproject-backend/app/controllers/transactions_controller.rb
+++ jsproject-backend/app/controllers/transactions_controller.rb
@@ -5,6 +5,10 @@ class TransactionsController < ApplicationController
     transaction = Transaction.new(description: params[:description], amount: params[:amount].to_i, category_id: params[:category_id])
     if transaction.save
       transaction.updateBudget
-      
+      options= {include: [:budget]}
+      render json: CategorySerializer.new(transaction.category, options)
+    else
+      render json: {type: "error", message: "Did not process!"}
+    end
   end
 end
